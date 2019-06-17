5962fa406344abc0e79676c8c0a5a4ed2554cf62
megha <megha.patel2@sheridancollege.ca>
2019-06-17 10:32:06 -0400

    midtermcode

--- src/midtermexam_partC/UserProfile.java
+++ src/midtermexam_partC/UserProfile.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package midtermexam_partB;
+package midtermexam_partC;
 
 /**
  * The user profile for the OnlineMovie system. The user has a userID
